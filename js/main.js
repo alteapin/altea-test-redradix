727dcdfe6ad1e7befc49d72442998337b6a4f8fb
alteapin <alteap18@gmail.com>
2019-01-09 22:08:30 +0100

    final version

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-for (let i = 0; i < 15; i++) {
-    if (i % 3 === 0) {
-        console.log('Fizz');
-    } else if (i % 5 === 0) {
-        console.log('Buzz');
-    } else if (i % 15 === 0) {
-        console.log('FizzBuzz')
-    } else {
-        console.log(i)
-    }
+for (let i = 0; i < 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
 };
